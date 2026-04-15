33ceec51c2fcc566dfd58effa93b746223bfa0fa
Nur Muhamad Alwiansyah <2411102441080@umkt.ac.id>
2026-04-15 18:55:16 +0700

    refactor: perbaiki struktur kode kasir sesuai dengan prinsip clean code

--- kasir.py
+++ kasir.py
@@ -1,10 +1,22 @@
-# Kode Asli
-def p(x, y, z):
-    a = x * y
-    a = a + (a * 0.11)
-    if a > 1000000:
-        a = a - 50000
-    print("Total: ", a)
-    b = z - a
-    print("Kembali: ", b)
-    return a, b
+#kode perbaikan
+pajak_PPN=0.11
+diskon_maksimal=50000
+batas_diskon=100000
+def hitung_total_harga(harga_barang, jumlah_barang):
+    """menghitung total harga barang beserta PPN"""
+    subtotal = harga_barang * jumlah_barang
+    total_pajak = subtotal * pajak_PPN
+    return subtotal + total_pajak
+
+def terapkan_diskon(total_harga):
+    """memberikan diskon jika melebihi batas minimum pembelian"""
+    if total_harga>batas_diskon:
+        return total_harga - diskon_maksimal
+    return total_harga
+
+def cetak_struk(total_akhir,uang_bayar):
+    """mencetak total tagihan dan uang kembalian"""
+    kembalian = uang_bayar - total_akhir
+    print(f"total bayar : Rp {total_akhir}")
+    print(f"Kembalian : Rp {kembalian}")
+    return kembalian
